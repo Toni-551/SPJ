8ceff5743f1b0702655a6d45a718585ff7acbbd4
Toni Crnković <toni.crnkovic5@gmail.com>
2022-04-21 12:11:47 +0200

    Add file from lv1

---
+++ LV1/js/app.js
@@ -0,0 +1,48 @@
+
+
+function Press(){
+    var naziv = document.getElementById("naziv").value;
+    var semestar = document.getElementById("semestar").value;
+    var ects = document.getElementById("ects").value;
+    var table = document.getElementById("table");
+    var string = "<tr><td>"+naziv+"</td><td>"+semestar+"</td><td>"+ ects+"</td><td onclick='DeleteRow()'><button>d</button></td><td><input onclick='CheckPressCheckbox()' type='checkbox'></td></tr>"
+    table.innerHTML += string;
+}
+function DeleteRow(){
+    var td = event.target.parentNode;
+    var tr = td.parentNode;
+    tr.parentNode.removeChild(tr);
+}
+
+function CheckPressCheckbox(){
+    var checkbox = event.target.checked;
+    var td = event.target.parentNode;
+    var tr = td.parentNode;
+
+    if(checkbox == true){
+        tr.style.backgroundColor = "Green";
+    }else{
+        tr.style.backgroundColor = "transparent";
+    }
+}
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("serc");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
